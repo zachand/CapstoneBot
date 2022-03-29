856882e4a188bda86939d4d34be6cdec8bbe80e0
Zachary Andrews <hiflierza@gmail.com>
2022-03-29 18:49:48 -0500

    initial commit

---
+++ CapstoneBot.js
@@ -0,0 +1,4 @@
+const Discord = require("discord.js") //discord library to use discord api
+const fs = require('fs')              //filesystem library so I don't have to upload my bot token to github.
+const client = new Discord.Client()   //make a client for us to connect with
+
